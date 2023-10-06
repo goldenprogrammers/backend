3a019b2f42b957460647b6e71faa931ee3d1f7f3
Horiel Correa Costa <horiel.costa@compasso.com.br>
2023-10-06 10:10:01 -0400

    feat: actionsMicroservice first commit

---
+++ actionsMicroservice/src/main/java/com/actionsMicroservice/ActionsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.actionsMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ actionsMicroservice/src/test/java/com/actionsMicroservice/ActionsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actionsMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
