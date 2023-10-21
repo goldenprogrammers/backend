dbc1a96fc74ab907d10cb5d81b38acaa7adfafa0
Murilo Matias <m.matias@ufms.br>
2023-10-21 11:51:06 -0400

    feat: add basic structure for microservice

---
+++ certificateMicroservice/src/main/java/com/certificatemicroservice/certificateMicroservice/CertificateMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.certificatemicroservice.certificateMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateMicroserviceApplication.class, args);
+	}
+
+}
